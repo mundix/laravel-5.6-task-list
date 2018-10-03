9e29d152af6258196ceb127c2f6eb35cec7c25ff
Edmundo Pichardo <ce.pichardo@gmail.com>
2018-10-03 11:05:23 -0400

    Setup all  controller, api.php, migration

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        User::create(['name' => 'Edmundo Pichardo',
+        'email' => 'ce.pichardo@gmail.com',
+        'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+        'remember_token' => str_random(10),]);
     }
 }
